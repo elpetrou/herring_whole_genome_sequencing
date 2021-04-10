cba46edee2687538fe13d910cd2f5c7368b7bf67
Eleni L Petrou <elpetrou@gmail.com>
2021-04-09 17:31:22 -0700

    Update trimmomatic_sbatch.sh

--- Scripts/trimmomatic_sbatch.sh
+++ Scripts/trimmomatic_sbatch.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=elp_08
+#SBATCH --job-name=elp_09
 #SBATCH --account=merlab
 #SBATCH --partition=compute-hugemem
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
 ## Walltime (days-hours:minutes:seconds format)
 #SBATCH --time=2-12:00:00
 ## Memory per node
-#SBATCH --mem=100G
+#SBATCH --mem=80G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elpetrou@uw.edu
 
 
 ##### ENVIRONMENT SETUP ##########
-DATADIR=/mmfs1/gscratch/scrubbed/elpetrou/fastq/WA_herring
+DATADIR=/mmfs1/gscratch/scrubbed/elpetrou/fastq/AK_herring
 SAMPLELIST=trimmomatic_list.txt
 SUFFIX1=_R1_001.fastq # Suffix to raw fastq files. The forward reads with paired-end data.
 SUFFIX2=_R2_001.fastq # Suffix to raw fastq files. The reverse reads with paired-end data. 
@@ -25,7 +26,7 @@ OUTDIR=/mmfs1/gscratch/scrubbed/elpetrou/fastq_trimmed #where to store output fi
 mkdir $OUTDIR
 
 cd $DATADIR
-ls *$SUFFIX1 > $SAMPLELIST  
+ls *$SUFFIX1 > $SAMPLELIST
 
 ##############################################################################
 ## Run Trimmomatic. For an explanation of trimmomatic commands, see: 
@@ -33,22 +34,25 @@ ls *$SUFFIX1 > $SAMPLELIST
 ## https://datacarpentry.org/wrangling-genomics/03-trimming/index.html
 
 
-for infile in `cat $SAMPLELIST` 
+for infile in `cat $SAMPLELIST`
 do
-	base=`basename --suffix=$SUFFIX1 $infile`
-	trimmomatic PE \
-	-threads 1 -phred33 \
-	${infile} \
-	${base}$SUFFIX2 \
-	${base}_R1_001.trim.fastq \
-	${base}_R1_001.unpaired.trim.fastq \
-	${base}_R2_001.trim.fastq \
-	${base}_R2_001.unpaired.trim.fastq \
-	ILLUMINACLIP:$ADAPTERFILE:2:30:10:1:true \
-	SLIDINGWINDOW:4:20 \
-	MINLEN:40 
+        base=`basename --suffix=$SUFFIX1 $infile`
+        trimmomatic PE \
+        -threads 10 -phred33 \
+        ${infile} \
+        ${base}$SUFFIX2 \
+        ${base}_R1_001.trim.fastq \
+        ${base}_R1_001.unpaired.trim.fastq \
+        ${base}_R2_001.trim.fastq \
+        ${base}_R2_001.unpaired.trim.fastq \
+        ILLUMINACLIP:$ADAPTERFILE:2:30:10:1:true \
+        SLIDINGWINDOW:4:20 \
+        MINLEN:40
 done
 
+## Yaaay! This code is running!!
+## Here is an example of some output: Input Read Pairs: 6825779 Both Surviving: 5932557 (86.91%) Forward Only Surviving: 468865 (6.87%) Reverse Only Surviving: 215887 (3.16%) Dropped: 208470 (3.05%)
+## For one set of fastq samples, I think it took about 5 min. So it will take about a day to finish trimming one lane on Klone (give it two days as buffer)
 #############################################################################
 ## Move the results files to the output directory
 
