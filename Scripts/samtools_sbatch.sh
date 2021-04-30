6c9c7554376865e9f88156c5c55aedb9e5495e80
Eleni L Petrou <elpetrou@gmail.com>
2021-04-30 14:43:22 -0700

    Update samtools_sbatch.sh

--- Scripts/samtools_sbatch.sh
+++ Scripts/samtools_sbatch.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 ## Walltime (days-hours:minutes:seconds format)
-#SBATCH --time=4-12:00:00
+#SBATCH --time=3-12:00:00
 ## Memory per node
 #SBATCH --mem=100G
 #SBATCH --mail-type=ALL
@@ -13,34 +13,34 @@
 
 
 ##### ENVIRONMENT SETUP ##########
-# Specify the directory containing data
+## Specify the directory containing data
 DATADIR=/mmfs1/gscratch/scrubbed/elpetrou/bam #directory with sam files
 SUFFIX1=.sam #file suffix
 MYCONDA=samtools_env #name of the conda environment that has samtools
 
-# activate the conda environment that runs samtools
+## activate the conda environment that runs samtools
 source activate $MYCONDA
 
 
 ###################################################################################################################
-#Move into the working directory and run script
+## Move into the working directory and run script
 cd $DATADIR
 
-# Run samtools commands. This takes about 5 min per sample (so like 2 days total for whole data set?)
+## Run samtools commands. This takes about 5 min per sample (so like 2 days total for whole data set?)
 for MYSAMPLEFILE in *$SUFFIX1
 do
     echo $MYSAMPLEFILE
     MYBASE=`basename --suffix=$SUFFIX1 $MYSAMPLEFILE`
-    echo $MYBASE
-    samtools view -b -h -q 20 -F 4 $MYSAMPLEFILE | \
-    samtools sort - | \
-    samtools index - $MYBASE'_sorted.bam'
+    samtools view -bS -F 4 $MYBASE'.sam' > $MYBASE'.bam'
+    samtools view -h -q 20 $MYBASE'.bam' | samtools view -buS - | samtools sort -o $MYBASE'_minq20_sorted.bam'
+    samtools index $MYBASE'_minq20_sorted.bam'
 done
 
-# Flag explanations for samtools view:
-# -b       output BAM
-# -h       include header in SAM output
-# -q INT   only include reads with mapping quality >= INT [0]
-#-F INT   only include reads with none of the bits set in INT set in FLAG [0] (aka when this is set to 4, you remove unmapped reads)
+## Flag explanations for samtools view:
+## -b       output BAM
+## -h       include header in SAM output
+## -q INT   only include reads with mapping quality >= INT [0]
+##-F INT   only include reads with none of the bits set in INT set in FLAG [0] (aka when this is set to 4, you remove unmapped reads)
+
 
 
